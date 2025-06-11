8042eab342b88c154a2a88ca880a805765387896
Afnan Abo-Asal <afnanalaa49@gmail.com>
2025-06-11 12:32:05 +0300

    Uploading Project Files

---
+++ MVC_Task1/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Task1.Data;
+
+namespace MVC_Task1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public ViewResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+        public ViewResult Details(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+    }
+}
---
+++ MVC_Task1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Task1.Models;
+
+namespace MVC_Task1.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=IMPLUTOGAL;Database=MVC_Task1;Trusted_Connection=True;TrustServerCertificate = True");
+        }
+    }
+}
